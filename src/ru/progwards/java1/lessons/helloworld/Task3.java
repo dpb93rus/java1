c8870017e0465846c8edb9d047176d4c3d036a71
Dmitry <dpb93rus@gmail.com>
2019-11-06 21:29:11 +0300

    HomeWork1

---
+++ src/ru/progwards/java1/lessons/helloworld/Task3.java
@@ -0,0 +1,39 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task3 {
+    public static int addition(int x, int y) {
+        System.out.println("Вызвана функция addition()");
+        return (x + y);
+    }
+
+    public static int subtraction(int x, int y) {
+        System.out.println("Вызвана функция subtraction()");
+        return (x - y);
+    }
+
+    public static int multiplication(int x, int y) {
+        System.out.println("Вызвана функция multiplication()");
+        return (x * y);
+    }
+
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 34;
+        b = 55;
+        System.out.print("a=");
+        System.out.println(34);
+        System.out.print("b=");
+        System.out.println(55);
+        c = addition(a, b);
+        System.out.print("a+b=");
+        System.out.println(c);
+        c = subtraction(a, b);
+        System.out.print("a-b=");
+        System.out.println(c);
+        c = multiplication(a, b);
+        System.out.print("a*b=");
+        System.out.println(c);
+    }
+}
+}
+
