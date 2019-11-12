f93b3310e4f4505180c6ecaf20df36ea6b6c88b0
Dmitry <dpb93rus@gmail.com>
2019-11-12 15:31:24 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
     public static double volumeBallDouble(double radius){
-        double v = 4/3*3.14*radius*radius*radius;
+        double v = 4.0/3.0*3.14*radius*radius*radius;
         return v;
     }
     public static float volumeBallFloat(float radius){
