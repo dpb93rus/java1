b395d6182e7721dbf60830e22821248de3103d0b
Dmitry <dpb93rus@gmail.com>
2019-11-26 22:34:38 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -2,11 +2,31 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class Binary {
     byte num = 0;
-    public Binary(byte num){
+
+    public Binary(byte num) {
         this.num = num;
     }
+
     @Override
-    public String toString(){
-        return
+    public String toString() {
+        String result = "";
+        int c = 0;
+        while (c < 8) {
+            if ((num & 1) == 1)
+                result = ("1" + result);
+            else
+                result = ("0" + result);
+            num >>= 1;
+            c++;
+        }
+
+            return result;
+
+        }
+        public static void main (String[]args){
+            Binary bin = new Binary((byte) 128);
+            System.out.println(bin);
+
+        }
     }
-}
+
