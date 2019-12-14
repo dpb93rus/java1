dee6adaac80619fb3089f15901d185f60774b72f
Dmitry <dpb93rus@gmail.com>
2019-12-14 20:56:42 +0300

    HomeWork1

--- src/ru/progwards/dpb/Rectangle.java
+++ src/ru/progwards/dpb/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.progwards.dpb;
 
+import java.util.Objects;
+
 public class Rectangle {
     private double a;
     private double b;
@@ -12,6 +14,20 @@ public class Rectangle {
     public double area() {
         return a*b;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(rectangle.a, a) == 0 && Double.compare(rectangle.b, b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
     public int compareTo (Rectangle anRectangle) {
         if (this.area()==anRectangle.area())
             return 0;
--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -1,25 +1,32 @@
 package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 public class DIntArray {
-    private int a[];
-    int n = 0;
+    private int a[] = {};
+
     DIntArray () {
     }
     public void add(int num){
-        int added[] = new int [n+1];
+        int [] b = new int [a.length+1];
+        b[b.length-1] = num;
+        a = b;
     }
     public void atInsert(int pos, int num) {
-
+        int [] b = new int [a.length+1];
+        System.arraycopy(a , 0 , b , pos - 1, b.length);
+        System.arraycopy(a , pos + 1 , b , a.length, b.length);
+        b[pos] = num;
+        a = b;
     }
     public void atDelete(int pos) {
-
+        int [] b = new int [a.length-1];
+        System.arraycopy(a , 0 , b , pos - 1, b.length);
+        System.arraycopy(a , pos + 1 , b , a.length, b.length);
+        a = b;
     }
     public int at(int pos) {
-    return 0;
+    return a[pos];
     }
 
     public static void main(String[] args) {
-        int n = 0;
-
     }
 }
