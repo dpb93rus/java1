35fdee0999c369e6dd1fe52c6220d690926e8e53
Dmitry <dpb93rus@gmail.com>
2020-01-22 19:35:50 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -6,10 +6,14 @@ import java.util.Scanner;
 public class LineCount {
     public static int calcEmpty(String fileName) {
         int i = 0;
-        try (FileReader reader = new FileReader(fileName); Scanner scanner = new Scanner(reader)) {
-            while (scanner.hasNextLine()) {
+        try (FileReader reader = new FileReader(fileName);
+             Scanner scanner = new Scanner(reader)) {
+            do {
+                String s = scanner.nextLine();
+                if (s == "") i++;
                 scanner.nextLine();
             }
+            while (scanner.hasNextLine());
         } catch (Exception e) {
             return -1;
         }
