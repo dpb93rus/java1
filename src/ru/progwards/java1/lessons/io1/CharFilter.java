d08be582ac6ca91cfa3e02037eebd83375845544
Dmitry <dpb93rus@gmail.com>
2020-01-28 22:07:25 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,4 +1,31 @@
 package ru.progwards.java1.lessons.io1;
 
+
+import java.io.*;
+import java.util.*;
+
+
 public class CharFilter {
+
+    public static void filterFile(String inFileName, String outFileName, String filter) throws IOException {
+        FileReader reader = new FileReader(inFileName);
+        Scanner scanner = new Scanner(reader);
+        FileWriter writer = new FileWriter(outFileName, true);
+
+        StringBuffer S = new StringBuffer(scanner.nextLine());
+        for (int n = 1; n < S.length(); n++) {
+            if (((S.charAt(n)) + "").equals(filter)) {
+                S.deleteCharAt(n);
+                n--;
+            }
+        }
+        writer.write(String.valueOf(S));
+        reader.close(); scanner.close(); writer.close();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        filterFile("C:\\Users\\Dmitry\\IdeaProjects\\java1\\src\\ru\\progwards\\java1\\lessons\\io1\\fileName.txt",
+                "C:\\Users\\Dmitry\\IdeaProjects\\java1\\src\\ru\\progwards\\java1\\lessons\\io1\\file out","h");
+    }
 }
---
+++ src/ru/progwards/java1/lessons/io1/CharFilter2.java
@@ -0,0 +1,29 @@
+package ru.progwards.java1.lessons.io1;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class CharFilter2 {
+
+        public static int calcEmpty(String fileName) {
+            int i = 0;
+            try (FileReader reader = new FileReader(fileName);
+                 Scanner scanner = new Scanner(reader)) {
+                while (scanner.hasNextLine()) {
+                    String s = scanner.nextLine();
+                    if ("".equals(s))
+                        i++;
+                }
+
+            } catch (Exception e) {
+                return -1;
+            }
+
+            return i;
+        }
+
+    }
