14ebd8014cc194bd8dcfe5425cd6d17020c4c740
Dmitry <dpb93rus@gmail.com>
2020-02-16 23:57:56 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -1,7 +1,7 @@
 package ru.progwards.java1.lessons.io2;
 
-import java.io.FileReader;
-import java.io.FileWriter;
+        import java.io.FileReader;
+        import java.io.FileWriter;
 
 public class Censor {
 
@@ -13,46 +13,39 @@ public class Censor {
             info = original.getMessage();
             name = original.getLocalizedMessage();
         }
+
         @Override
         public String getMessage() {
             return ("inoutFileName:" + info);
         }
     }
-        public static void censorFile(String inoutFileName, String[] obscene) throws Throwable {
-//      Считывание
-            StringBuffer s = new StringBuffer("");
-            try (FileReader reader = new FileReader(inoutFileName)) {
-                for (int c; (c = reader.read()) != -1; ) {
-                    s.append((char) c);
-                }
-            } catch (Throwable EX) {
-                throw new CensorException(EX);
-            }
-//      Формируем выходной текст
-            for (int c = 0; c < obscene.length; c++) {
-                StringBuffer temp2 = new StringBuffer("");
-                if (s.indexOf(obscene[c]) != -1) {
-                    for (int g = 0; g < obscene[c].length(); g++) {
-                        temp2.append("*");
-                    }
-                    s.replace(s.indexOf(obscene[c]), s.indexOf(obscene[c]) + obscene[c].length(), temp2.toString());
-                }
-                if (s.indexOf(obscene[c]) != -1) c--;
-            }
-            try (FileWriter writer = new FileWriter(inoutFileName)) {
-                writer.write(s.toString());
-            } catch (Throwable EX) {
-                throw new CensorException(EX);
-            }
-        }
 
-        public static void main(String[] args) throws Throwable {
-            String A[] = new String[]{"Java", "Oracle", "Sun", "Microsystems"};
-            try {
-                censorFile("C:\\Users\\Dmitry\\IdeaProjects\\java1\\src\\ru\\progwards\\java1\\lessons\\io2\\filin.txt", A);
-            } catch (CensorException EX) {
-                System.err.println(EX.getMessage());
+    public static void censorFile(String inoutFileName, String[] obscene) throws Throwable {
+//      Считывание
+        StringBuffer s = new StringBuffer("");
+        try (FileReader reader = new FileReader(inoutFileName)) {
+            for (int c; (c = reader.read()) != -1; ) {
+                s.append((char) c);
             }
+        } catch (Throwable EX) {
+            throw new CensorException(EX);
+        }
+//      Формируем выходной текст
+        for (int c = 0; c < obscene.length; c++) {
+            StringBuffer temp2 = new StringBuffer("");
+            if (s.indexOf(obscene[c]) != -1) {
+                for (int g = 0; g < obscene[c].length(); g++) {
+                    temp2.append("*");
+                }
+                s.replace(s.indexOf(obscene[c]), s.indexOf(obscene[c]) + obscene[c].length(), temp2.toString());
+            }
+            if (s.indexOf(obscene[c]) != -1) c--;
+        }
+        try (FileWriter writer = new FileWriter(inoutFileName)) {
+            writer.write(s.toString());
+        } catch (Throwable EX) {
+            throw new CensorException(EX);
         }
     }
+}
 
