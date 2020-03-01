d5c4c3a7511f9ac9eeb41a948f9cfbb536095787
Dmitry <dpb93rus@gmail.com>
2020-03-01 19:41:41 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -1,24 +1,36 @@
 package ru.progwards.java1.lessons.collections;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayIterator<T> implements Iterator<T> {
 
     private T[] array;
+    private int pos;
 
     ArrayIterator(T[] array) {
         this.array = array;
+        this.pos = 0;
     }
 
     @Override
     public boolean hasNext() {
-        // TODO Auto-generated method stub
+        if (pos < array.length) return true;
         return false;
     }
 
     @Override
     public T next() {
-        // TODO Auto-generated method stub
+        if (this.hasNext() == true) {
+            pos++;
+            return array[pos - 1];
+        }
         return null;
+    }
+
+    public static void main(String[] args) {
+        Integer[] A = {12, 1, 214, 3242, 4, 234, 24, 2, 4, 34, 432, 4, 23, 42, 43, 432, 432, 243, 4322, 43, 432,};
+        ArrayIterator<Integer> iterator = new ArrayIterator<>(A);
+        System.out.println(iterator.hasNext());
     }
 }
