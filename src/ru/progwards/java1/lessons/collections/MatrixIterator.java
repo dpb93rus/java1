043e6538e05a87466ada11202ceab005babbcea4
Dmitry <dpb93rus@gmail.com>
2020-03-01 21:54:12 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.Iterator;
 
-public class MatrixIterator <T> implements Iterator<T> {
+public class MatrixIterator<T> implements Iterator<T> {
 
 
     private T[][] array;
@@ -15,21 +15,60 @@ public class MatrixIterator <T> implements Iterator<T> {
         for (int n = 0; n < array.length; n++) {
             g += array[n].length;
         }
-        arr = (T[])new Object[g];
-        this.pos = 0;
-    }
-    @Override
-    public boolean hasNext() {
-        if (pos < this.arr.length) return true;
-        return false;
-    }
-
-    @Override
-    public T next() {
-        if (this.hasNext() == true) {
-            pos++;
-            return arr[pos - 1];
+        this.arr = (T[]) new Object[g];
+        for (int l = 0; l < arr.length; l++) {
+            for (int n = 0; n < array.length; n++) {
+                for (int m = 0; m < array[n].length; m++) {
+                    arr[l] = array[n][m];
+                    l++;
+                }
+            }
         }
-        return null;
+            this.pos = 0;
+        }
+        @Override
+        public boolean hasNext () {
+            if (pos < this.arr.length) return true;
+            return false;
+        }
+
+        @Override
+        public T next () {
+            if (this.hasNext() == true) {
+                pos++;
+                return arr[pos - 1];
+            }
+            return null;
+        }
+        public static void main (String[]args){
+            Integer[][] A = {{12, 1, 214}, {3242, 4, 234, 24}, {2, 4, 34, 432, 4, 23}, {42, 43, 432, 432, 243, 4322, 43, 432}};
+            MatrixIterator<Integer> iterator = new MatrixIterator<>(A);
+            System.out.println(iterator.hasNext());
+            int[][] B = {{12, 1}, {214, 3242}, {4, 8, 234, 8}, {2, 4}};
+            System.out.println(B[2].length);
+            System.out.println(A.toString());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+            System.out.println(iterator.next());
+
+
+        }
     }
-}
