4009947369721413dba5054cb3b597f3272ccfc1
Dmitry <dpb93rus@gmail.com>
2020-03-02 23:03:38 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -1,38 +1,76 @@
-//package ru.progwards.java1.lessons.sets;
-//
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Set;
-//
-//public class ProductAnalytics {
-//    private List<Shop> shops;
-//    private List<Product> products;
-//
-//    public ProductAnalytics(List<Product> products, List<Shop> shops) {
-//        this.products = products;
-//        this.shops = shops;
-//    }
-//
-//    public Set<Product> existInAll() {
-//HashSet pr = new HashSet(this.products);
-//HashSet sh = new HashSet(this.shops);
-//        for (HashSet<shop> temp:sh) {
-//
-//        }
-//
-//    }
-//
-//    public Set<Product> existAtListInOne() {
-//
-//    }
-//
-//    public Set<Product> notExistInShops() {
-//
-//    }
-//
-//    public Set<Product> existOnlyInOne() {
-//
-//    }
-//
-//
-//}
+package ru.progwards.java1.lessons.sets;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ProductAnalytics {
+    private List<Shop> shops;
+    private List<Product> products;
+
+    public ProductAnalytics(List<Product> products, List<Shop> shops) {
+        this.products = products;
+        this.shops = shops;
+    }
+
+    public Set<Product> existInAll() {
+        HashSet pr = new HashSet(this.products);
+        for (Shop temp: shops) {
+            pr.retainAll(temp.getProducts());
+        }
+        return pr;
+
+    }
+    public static HashSet<Integer> union(HashSet<Integer> set1, HashSet<Integer> set2){
+        HashSet <Integer>res = new HashSet<>(set1);
+        res.addAll(set2);
+        return res;
+
+    }
+    public static HashSet<Integer> difference(HashSet<Integer> set1, HashSet<Integer> set2) {
+        HashSet res = new HashSet(set1);
+        res.removeAll(set2);
+        return res;
+    }
+
+    public static HashSet<Integer> intersection(HashSet<Integer> set1, HashSet<Integer> set2){
+        HashSet res = new HashSet(set1);
+        res.retainAll(set2);
+        return res;
+
+    }
+    public static HashSet<Integer> symDifference(HashSet<Integer> set1, HashSet<Integer> set2) {
+        return difference(union(set1,set2),intersection(set1,set2));
+    }
+
+    public Set<Product> existAtListInOne() {
+        HashSet result = new HashSet();
+        for (Shop temp: shops) {
+            HashSet pr = new HashSet(this.products);
+            pr.retainAll(temp.getProducts());
+            result.addAll(pr);
+        }
+        return result;
+    }
+
+    public Set<Product> notExistInShops() {
+        HashSet pr = new HashSet(this.products);
+        for (Shop temp: shops) {
+            pr.removeAll(temp.getProducts());
+        }
+        return pr;
+    }
+
+    public Set<Product> existOnlyInOne() {
+        HashSet pr = new HashSet(this.products);
+        HashSet res = new HashSet();
+        for (Shop temp: shops) {
+            HashSet res1 = new HashSet(temp.getProducts());
+            res=symDifference(res,res1);
+        }
+        pr.retainAll(res);
+        return pr;
+    }
+
+
+}
