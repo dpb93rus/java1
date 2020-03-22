3e2245352f8ca15dac8aa118db33a6bd6d62a114
Dmitry <dpb93rus@gmail.com>
2020-03-22 20:41:18 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/queues/Calculate.java
+++ src/ru/progwards/java1/lessons/queues/Calculate.java
@@ -1,14 +1,18 @@
-//package ru.progwards.java1.lessons.queues;
-//
-//import java.util.Collection;
-//
-//public class Calculate {
-//
-//    public static double calculation1() {
-//
-//    }
-//
-//    public static double calculation2() {
-//
-//    }
-//}
+package ru.progwards.java1.lessons.queues;
+
+        import java.util.Collection;
+
+public class Calculate {
+
+    public static double calculation1() {
+       return  2.2*(3+12.1);
+    }
+
+    public static double calculation2() {
+        return (737.22+24)/(55.6-12.1)+(19-3.33)*(87+2*(13.001-9.2));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/ru/progwards/java1/lessons/queues/StackCalc.java
+++ src/ru/progwards/java1/lessons/queues/StackCalc.java
@@ -1,33 +1,16 @@
-//package ru.progwards.java1.lessons.queues;
-//
-//public class StackCalc {
-//    public void push(double value) {
-//
-//    }
-//
-//    public double pop() {
-//
-//    }
-//
-//    public void add() {
-//
-//    }
-//
-//    public void sub() {
-//
-//    }
-//
-//    public void mul(){
-//
-//    }
-//
-//    public void div() {
-//
-//    }
-//
-//
-//
-// }
+package ru.progwards.java1.lessons.queues;
+
+import java.util.ArrayDeque;
+
+public class StackCalc {
+    ArrayDeque<Double> A = new ArrayDeque<>();
+    public void push(double value) { A.push(value); }
+    public double pop() { return A.pop(); }
+    public void add() { A.push(A.pop() + A.pop()); }
+    public void sub() { double t = A.pop(); A.push(A.pop()-t); }
+    public void mul(){ A.push(A.pop() * A.pop()); }
+    public void div() { double t = A.pop(); A.push(A.pop()/t); }
+ }
 
 
 
