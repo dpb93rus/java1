3290ec405f959234ad7647fb371939bc7e30fb90
Dmitry <dpb93rus@gmail.com>
2020-03-22 20:19:13 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/queues/Order.java
+++ src/ru/progwards/java1/lessons/queues/Order.java
@@ -1,18 +1,30 @@
 package ru.progwards.java1.lessons.queues;
 
+import java.util.ArrayDeque;
+
 public class Order {
     private double sum;
-    private int num;
+    public int pr;
+    private int num = 0;
+    private static int c;
+
+
     public Order (double sum) {
-        int c = 0;
-        c++;
-        this.num = c;
         this.sum = sum;
-    }
+        if (this.getSum() <= 10000.0) pr = 3;
+        if (this.getSum() > 10000.0 & this.getSum() <= 20000.0) pr = 2;
+        if (this.getSum() > 20000.0) pr = 1;
+        c++;
+        num+=c;
+        }
+
     public double getSum(){
 return this.sum;
     }
     public  int getNum() {
         return this.num;
+    }
+    public String toString () {
+        return ("Num = "+ this.getNum() +"\n"+"Sum = "+ this.getSum() +"\n"+"Prior = "+ this.pr +"\n");
     }
 }
--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -1,11 +1,48 @@
-//package ru.progwards.java1.lessons.queues;
-//
-//public class OrderQueue {
-//    public void add(Order order) {
-//
-//    }
-//
-//    public Order get() {
-//
-//    }
-//}
+package ru.progwards.java1.lessons.queues;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class OrderQueue {
+    Comparator<Order> c = new Comparator<Order>() {
+        @Override
+        public int compare(Order o1, Order o2) {
+            if (o1.pr == o2.pr) {
+                if (o1.getNum() > o2.getNum()) return 1;
+                if (o1.getNum() < o2.getNum()) return -1;
+            }
+            if (o1.pr > o2.pr) return 1;
+            if (o1.pr < o2.pr) return -1;
+            return 0;
+        }
+    };
+    PriorityQueue<Order> Q = new PriorityQueue(c);
+    private Order o;
+    public void add(Order order) {
+        Q.add(order);
+    }
+
+    public Order get() {
+        return Q.poll();
+    }
+
+    public static void main(String[] args) {
+        Order o1 = new Order(23456);
+        Order o2 = new Order(2346);
+        Order o3 = new Order(18456);
+        Order o4 = new Order(124);
+        Order o5 = new Order(12000);
+        Order o6 = new Order(12000);
+        Order o7 = new Order(12000);
+        OrderQueue TEST = new OrderQueue();
+        TEST.add(o1);
+        TEST.add(o2);
+        TEST.add(o3);
+        TEST.add(o6);
+        TEST.add(o7);
+        TEST.add(o4);
+        TEST.add(o5);
+        System.out.println(TEST.get());
+        System.out.println(TEST.get());System.out.println(TEST.get());System.out.println(TEST.get());System.out.println(TEST.get());System.out.println(TEST.get());System.out.println(TEST.get());System.out.println(TEST.get());System.out.println(TEST.get());
+    }
+}
