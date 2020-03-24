755553373474c561368000cffddc6f3a7849b7b9
Dmitry <dpb93rus@gmail.com>
2020-03-24 17:48:21 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -6,53 +6,56 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FiboMapCache {
-     boolean cacheOn;
-     private Map<Integer, BigDecimal> fiboCache;
+    boolean cacheOn;
+    private Map<Integer, BigDecimal> fiboCache;
 
-     public FiboMapCache(boolean cacheOn) {
-          this.cacheOn = cacheOn;
-     }
+    public FiboMapCache(boolean cacheOn) {
+        this.cacheOn = cacheOn;
+    }
 
-     public BigDecimal fiboNumber(int n) {
-          if ((fiboCache==null)) fiboCache = new HashMap<Integer, BigDecimal>();
-          if ((cacheOn)&(fiboCache.containsKey(n))) return fiboCache.get(n);
-          BigDecimal x = new BigDecimal("0");
-          BigDecimal y = new BigDecimal("1");
-          BigDecimal z = new BigDecimal("0");
-          int count = 1;
-          while (n != count) {
-               z = x;
-               x = y;
-               y = y.add(z);
-               count++;
-          }
-          fiboCache.put(n, y);
-          return y;
-     }
-     public void clearCahe() {
-//          fiboCache = null;
-          }
+    public BigDecimal fiboNumber(int n) {
+        if (fiboCache == null)
+             fiboCache = new HashMap<>();
+        if (cacheOn & fiboCache.containsKey(n))
+             return fiboCache.get(n);
+        BigDecimal x = BigDecimal.ZERO;
+        BigDecimal y = BigDecimal.ONE;
+        BigDecimal z = BigDecimal.ZERO;;
+        int count = 1;
+        while (n != count) {
+            z = x;
+            x = y;
+            y = y.add(z);
+            count++;
+        }
+        if (cacheOn) fiboCache.put(n, y);
+        return y;
+    }
 
-     public static void test() {
-          FiboMapCache cashOn = new FiboMapCache(true);
-          FiboMapCache cashOff = new FiboMapCache(false);
-          long a = System.nanoTime();
-          for (int n = 1; n <= 1000; n++) cashOff.fiboNumber(n);
-          a = System.nanoTime() - a;
-          long b = System.nanoTime();
-          for (int n = 1; n <= 1000; n++) cashOn.fiboNumber(n);
-          b = System.nanoTime() - b;
+    public void clearCahe() {
+        fiboCache = null;
+    }
 
+    public static void test() {
+        FiboMapCache cashOn = new FiboMapCache(true);
+        FiboMapCache cashOff = new FiboMapCache(false);
+        long b = System.nanoTime();
+        for (int n = 1; n <= 1000; n++) cashOn.fiboNumber(n);
+        for (int n = 1; n <= 1000; n++) cashOn.fiboNumber(n);
+        b = System.nanoTime() - b;
+        cashOff.clearCahe();
+        long a = System.nanoTime();
+        for (int n = 1; n <= 1000; n++) cashOff.fiboNumber(n);
+        for (int n = 1; n <= 1000; n++) cashOff.fiboNumber(n);
+        a = System.nanoTime() - a;
 
+        System.out.println("fiboNumber cacheOn=" + cashOn.cacheOn + " время выполнения     " + b);
+        System.out.println("fiboNumber cacheOn=" + cashOff.cacheOn + " время выполнения    " + a);
 
+    }
 
+    public static void main(String[] args) {
+        test();
 
-          System.out.println("fiboNumber cacheOn="+b);
-          System.out.println("fiboNumber cacheOff="+a);
-
-     }
-
-     public static void main(String[] args) {
-     test();
-     }
+    }
 }
--- src/ru/progwards/java1/lessons/maps/UsageFrequency.java
+++ src/ru/progwards/java1/lessons/maps/UsageFrequency.java
@@ -3,27 +3,30 @@
 //import java.io.FileNotFoundException;
 //import java.io.FileReader;
 //import java.io.IOException;
+//import java.util.HashMap;
 //import java.util.Map;
 //
 //public class UsageFrequency {
+//    StringBuffer D = new StringBuffer(""); String C;// Переменные класса принимающие строку
 //    public void processFile(String fileName) throws Exception {
 //        try {
 //            FileReader reader = new FileReader(fileName);
-//            StringBuffer D = new StringBuffer("");
-//            for (int c; (c = reader.read()) != -1; ) {
-//                D.append(c);
-//            }
-//    } catch (Throwable e) {
-//            throw new Exception() ;
+//            for (int c; (c = reader.read()) != -1; ) D.append(c);
+//            C = new String(D);
+//        } catch (Throwable e) {
+//            throw new Exception();
 //        }
+//    }
 //
-//        public Map<Character, Integer> getLetters(){
+//        public Map<Character, Integer> getLetters() {
+//            Map<Character, Integer> = new HashMap<>();
+//            char [] a = C.toCharArray();
 //
 //    }
 //    public Map<String, Integer> getWords() {
 //
 //    }
 //
-
-
-//}
+//
+//
+////}
