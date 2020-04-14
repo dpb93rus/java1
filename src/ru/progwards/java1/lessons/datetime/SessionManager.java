3a64df53b2a90c722785b392184fd41204b57f63
Dmitry <dpb93rus@gmail.com>
2020-04-15 00:19:12 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -1,35 +1,53 @@
-//package ru.progwards.java1.lessons.datetime;
-//
-//import java.util.ArrayList;
-//
-//public class SessionManager {
-//
-//    private ArrayList sessions;
-//    private int sessionValid;
-//
-//    public SessionManager(int sessionValid) {
-//
-//    }
-//
-//    public void add(UserSession userSession) {
-//
-//    }
-//
-//    public UserSession find(String userName) {
-//
-//    }
-//
-//    public UserSession get(int sessionHandle) {
-//
-//    }
-//
-//    public void delete(int sessionHandle) {
-//
-//    }
-//
-//    public deleteExpired() {
-//
-//    }
-//}
-//
+package ru.progwards.java1.lessons.datetime;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class SessionManager {
+
+    private ArrayList <UserSession> sessions;
+    private int sessionValid;
+
+    public SessionManager(int sessionValid) {
+    this.sessionValid = sessionValid;
+    }
+
+    public void add(UserSession userSession) {
+    sessions.add(userSession);
+    }
+
+    public UserSession find(String userName) {
+    for (UserSession temp: sessions) {
+        if (temp.getUserName().equals(userName)) return temp;
+    }
+        return null;
+    }
+
+    public UserSession get(int sessionHandle) {
+        for (UserSession temp: sessions) {
+            if ((temp.getSessionHandle()==(sessionHandle))&&
+                    (!LocalDateTime.now().isAfter(temp.getLastAccess().plusSeconds((long)temp.getSessionHandle()))))
+                    return temp;
+        }
+        return null;
+    }
+
+    public void delete(int sessionHandle) {
+        for (UserSession temp: sessions) {
+            if (temp.getSessionHandle()==sessionHandle) {
+                sessions.remove(temp);
+                break;
+            }
+
+        }
+    }
+
+    public void deleteExpired() {
+        for (UserSession temp: sessions) {
+            if (!LocalDateTime.now().isAfter(temp.getLastAccess().plusSeconds((long)temp.getSessionHandle()))&&
+                    (!LocalDateTime.now().isBefore(temp.getLastAccess())))  sessions.remove(temp);
+        }
+    }
+}
+
 
--- src/ru/progwards/java1/lessons/datetime/UserSession.java
+++ src/ru/progwards/java1/lessons/datetime/UserSession.java
@@ -1,30 +1,20 @@
-//package ru.progwards.java1.lessons.datetime;
-//
-//import java.time.LocalDateTime;
-//
-//public class UserSession {
-//
-//    private int sessionHandle;
-//    private String userName;
-//    private LocalDateTime lastAccess;
-//
-//    public getSessionHandle() {
-//
-//    }
-//    public getUserName(){
-//
-//    }
-//
-//    public getLastAccess() {
-//
-//    }
-//
-//    public updateLastAccess() {
-//
-//    }
-//    public UserSession(String userName) {
-//
-//    }
-//
-//
-//}
+package ru.progwards.java1.lessons.datetime;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class UserSession {
+    private int sessionHandle;
+    private String userName;
+    private LocalDateTime lastAccess;
+    public int getSessionHandle() { return sessionHandle; }
+    public String getUserName(){ return userName; }
+    public LocalDateTime getLastAccess() { return lastAccess; }
+    public void updateLastAccess() { this.lastAccess = LocalDateTime.now(); }
+    public UserSession(String userName) {
+        this.userName = userName;
+        lastAccess = LocalDateTime.now();
+        sessionHandle = new Random().nextInt();
+    }
+}
