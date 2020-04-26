20d2b098ffa3ff4d9f40af9629425e405b6165b5
Dmitry <dpb93rus@gmail.com>
2020-04-26 21:12:41 +0300

    HomeWork1

--- src/ru/progwards/java1/lessons/files/OrderItem.java
+++ src/ru/progwards/java1/lessons/files/OrderItem.java
@@ -3,23 +3,22 @@ package ru.progwards.java1.lessons.files;
 public class OrderItem {
 
     public String googsName;
-
     public int count;
-
     public double price;
-
-    @Override
-    public String toString() {
-        return "OI{" +
-                "gName='" + googsName + '\'' +
-                ", c=" + count +
-                ", pr=" + price +
-                '}';
-    }
-
     public OrderItem (String googsName, int count, double price) {
         this.count = count;
         this.googsName = googsName;
         this.price = price;
     }
+
+//    @Override
+//    public String toString() {
+//        return "OI{" +
+//                "gName='" + googsName + '\'' +
+//                ", c=" + count +
+//                ", pr=" + price +
+//                '}';
+//    }
+
+
 }
